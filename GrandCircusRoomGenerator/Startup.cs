671d8f04a86b0b31cabadcc10368154254406106
bradfordlauren <bradfordlauren@yahoo.com>
2016-09-26 15:00:29 -0400

    Add project files.

---
+++ GrandCircusRoomGenerator/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GrandCircusRoomGenerator.Startup))]
+namespace GrandCircusRoomGenerator
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
